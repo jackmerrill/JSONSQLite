cea85c4f1a51759142b8f9c8e20ca7aa99bc58ce
jmerrill <yoshifan509@gmail.com>
2018-10-05 17:47:26 +0000

    Need to fix getJSON, broken. Everything else works.

--- index.js
+++ index.js
@@ -2,20 +2,34 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('json.db'); // Don't change this.
 
 
-db.run("CREATE TABLE jsondata (info TEXT)")
+db.run("CREATE TABLE IF NOT EXISTS jsondata (data TEXT)")
 
 /*
 Functions
 */
 
-function add(data) {
-  
+const setJSON = function setJSON(id,data) {
+  var insert = db.prepare("UPDATE jsondata SET data = (?) WHERE rowid = (?)")
+  insert.run(data, id)
+  return console.log("Success! Set JSON data to "+data+" in row "+id)
 }
 
+exports.setJSON = setJSON
 
-db.serialize(function() {
-  db.run("CREATE TABLE jsondata (info TEXT)")
-  db.prepare("INSERT INTO jsondata VALUES (?)")
-});
- 
-db.close();
+const newJSON = function newJSON(data) {
+  var insert = db.prepare("INSERT INTO jsondata VALUES (?)")
+  insert.run(data)
+  return console.log("Success! Set NEW JSON data to "+data)
+}
+
+exports.newJSON = newJSON
+
+const getJSON = function getJSON() {
+  db.get("SELECT data FROM jsondata", function(err, row) {
+    if (err) throw err
+    return row
+  })
+}
+
+exports.getJSON = getJSON
+
---
+++ test/maintest.js
@@ -0,0 +1,6 @@
+const jsql = require('../index.js')
+jsql.newJSON('{ "test": "hello" }')
+
+jsql.setJSON(1, '{ "test": "hi" }')
+
+console.log(jsql.getJSON())
